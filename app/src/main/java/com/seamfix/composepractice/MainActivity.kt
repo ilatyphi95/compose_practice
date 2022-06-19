8c716298c329b943ca5774d983beeb70422c35c0
ilatyphi95 <Ilatyphi95@gmail.com>
2022-06-19 16:48:18 +0100

    Add colors

--- app/src/main/java/com/seamfix/composepractice/MainActivity.kt
+++ app/src/main/java/com/seamfix/composepractice/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,12 +15,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.seamfix.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposePracticeTheme {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
         }
     }
 }
@@ -27,17 +32,18 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row (modifier = Modifier.padding(all = 8.dp)){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.height(4.dp))
         Column {
-            Text(text = msg.author)
+            Text(text = msg.author, color = MaterialTheme.colors.secondaryVariant)
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
@@ -47,5 +53,7 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great"))
+    ComposePracticeTheme {
+        MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great"))
+    }
 }
