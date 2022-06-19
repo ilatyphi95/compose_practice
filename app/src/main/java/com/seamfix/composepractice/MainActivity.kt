81dab4c85c9cedea3a4eed8a6c1da56bc05f1cac
ilatyphi95 <Ilatyphi95@gmail.com>
2022-06-19 15:48:37 +0100

    Add multiline text

--- app/src/main/java/com/seamfix/composepractice/MainActivity.kt
+++ app/src/main/java/com/seamfix/composepractice/MainActivity.kt
@@ -11,18 +11,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "world")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name")
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Android")
+    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great"))
 }
