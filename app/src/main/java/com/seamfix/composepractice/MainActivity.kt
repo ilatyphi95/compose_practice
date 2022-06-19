d8d0ee6f1a94e3d20ee329b5825522352d6055f3
ilatyphi95 <Ilatyphi95@gmail.com>
2022-06-19 15:26:14 +0100

    Add first function

--- app/src/main/java/com/seamfix/composepractice/MainActivity.kt
+++ app/src/main/java/com/seamfix/composepractice/MainActivity.kt
@@ -3,12 +3,19 @@ package com.seamfix.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MessageCard(name = "world")
         }
     }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name")
 }
